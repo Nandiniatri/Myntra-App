d1c4c43df0ab2237b92077213b7aa30c4c633937
Nandiniatri <atrinandini8@gmail.com>
2025-04-28 15:28:43 +0530

    push updated code

--- src/myntraApp/header/Header.jsx
+++ src/myntraApp/header/Header.jsx
@@ -5,53 +5,53 @@ import Input from "../../components/Input";
 import { CiUser } from "react-icons/ci";
 import { CiHeart } from "react-icons/ci";
 import { BsHandbag } from "react-icons/bs";
-import './header.css'
+import './header.css';
 
 const Header = () => {
     const { headerCenterData, logo } = useContext(AllDataContext);
 
-    return (    
+    return (
         <header>
             <div className="header-logo-main-container">
                 {logo.map((headerLogo) => {
                     return (
                         <Image src={headerLogo.src} key={headerLogo.id} />
-                    )
+                    );
                 })}
             </div>
 
             <div className="header-UL-LI-main-container">
-                <ul>
-                    {headerCenterData.map((headerCen) => {
-                        return (
-                            <li key={headerCen.id}>{headerCen.title}</li>
-                        )
-                    })}
-                </ul>
+                {headerCenterData.map((headerCen) => {
+                    return (
+                        <a href={"#"} key={headerCen.id} className="header-nav-link">
+                            {headerCen.title}
+                        </a>
+                    );
+                })}
             </div>
 
             <div className="header-Input-mai-container">
-                <Input placeholder="Search for a product,brands and more" />
+                <Input placeholder="Search for a product, brands and more" />
             </div>
 
-            <div className="header-icons-main-divS">
-                <div>
-                    <CiUser />
-                    <h5>Profile</h5>
+            <div className="header-icons-main-container">
+                <div className="header-icon-div">
+                    <CiUser size={22} />
+                    <span>Profile</span>
                 </div>
 
-                <div>
-                    <CiHeart />
-                    <h5>Wishlist</h5>
+                <div className="header-icon-div">
+                    <CiHeart size={22} />
+                    <span>Wishlist</span>
                 </div>
 
-                <div>
-                    <BsHandbag />
-                    <h5>Bag</h5>
+                <div className="header-icon-div">
+                    <BsHandbag size={22} />
+                    <span>Bag</span>
                 </div>
             </div>
         </header>
-    )
-}
+    );
+};
 
 export default Header;
