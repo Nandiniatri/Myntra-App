d959b32ae06e5f077117ed0f71fd0db9e05aeb01
Nandiniatri <atrinandini8@gmail.com>
2025-04-29 19:53:10 +0530

    push updated code

--- src/Men/Men.jsx
+++ src/Men/Men.jsx
@@ -9,8 +9,8 @@ const Men = () => {
             <TopBrandGrid url='/data/shopData/Men/topBrandFile/TopBrand.json' />
             <CatagoryToBag url='/data/shopData/Men/catagoriesToBagFile/CatagoryTobag.json' />
             <CatagoryToBag url="/data/shopData/Men/exploreTopBrandsFile/ExploreTopBrands.json" />
-            <CatagoryToBag url="" />
-            <CatagoryToBag url="" />
+            <CatagoryToBag url="/data/shopData/Men/myntraLuxe/MyntraLuxe.json" />
+            <CatagoryToBag url="/data/shopData/Men/trendingInIndianWear/TrendingInIndianWear.json" />
             <CatagoryToBag url="" />
             <CatagoryToBag url="" />
             <CatagoryToBag url="" />
