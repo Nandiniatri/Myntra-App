9292edba4df2f893cc0bde620acaa8a22cf57e41
Nandiniatri <atrinandini8@gmail.com>
2025-04-29 21:56:41 +0530

    push updated code

--- src/Women/Women.jsx
+++ src/Women/Women.jsx
@@ -6,14 +6,14 @@ const Women = () => {
     return (
         <div>
             <WomenSlider />
-            <TopBrandGrid url='/data/shopData/Men/topBrandFile/TopBrand.json' />
-            <CatagoryToBag url='/data/shopData/Men/catagoriesToBagFile/CatagoryTobag.json' title="Categories To Bag" />
-            <CatagoryToBag url="/data/shopData/Men/exploreTopBrandsFile/ExploreTopBrands.json" title="Explore Top Brands" />
-            <CatagoryToBag url="/data/shopData/Men/myntraLuxe/MyntraLuxe.json" title="Myntra Luxe" />
-            <CatagoryToBag url="/data/shopData/Men/trendingInIndianWear/TrendingInIndianWear.json" title="Trending In Indian Wear" />
-            <CatagoryToBag url="/data/shopData/Men/trendingInSportsWear/TrendingInSportsWear.json" title="Trending In Sports Wear" />
-            <CatagoryToBag url="/data/shopData/Men/trendingInFootwear/TrendingInFootwear.json" title="Trending In Footwear" />
-            <CatagoryToBag url="/data/shopData/Men/trendingInAccessories/TrendingInAccessories.json" title="Trending In Accessories" />
+            <TopBrandGrid url='/data/shopData/Women/topBrandFile/TopBrand.json' />
+            <CatagoryToBag url='/data/shopData/Women/catagoriesToBagFile/CatagoryTobag.json' title="Categories To Bag" />
+            <CatagoryToBag url="/data/shopData/Women/exploreTopBrandsFile/ExploreTopBrands.json" title="Explore Top Brands" />
+            <CatagoryToBag url="/data/shopData/Women/myntraLuxe/MyntraLuxe.json" title="Myntra Luxe" />
+            <CatagoryToBag url="/data/shopData/Women/trendingInIndianWear/TrendingInIndianWear.json" title="Trending In Indian Wear" />
+            <CatagoryToBag url="/data/shopData/Women/trendingInSportsWear/TrendingInSportsWear.json" title="Trending In Sports Wear" />
+            <CatagoryToBag url="/data/shopData/Women/trendingInFootwear/TrendingInFootwear.json" title="Trending In Footwear" />
+            <CatagoryToBag url="/data/shopData/Women/trendingInAccessories/TrendingInAccessories.json" title="Trending In Accessories" />
         </div>
     )
 }
